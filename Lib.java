38b2c6566fcb6661833ff9737bd1e6ec750238c8
Sai Krishna Mikkilineni <saikrishnamikkilineni9@gmail.com>
2023-07-12 15:24:28 +0000

    Libarary Application

---
+++ Lib.java
@@ -0,0 +1,30 @@
+public class Lib {
+  String stu_title;
+  String stu_author;
+  String stu_category;
+
+  public void setStu_title(String stu_title) {
+    this.stu_title = stu_title;
+  }
+
+  public String getStu_title() {
+    return stu_title;
+  }
+
+  public void setStu_author(String stu_author) {
+    this.stu_author = stu_author;
+  }
+
+  public String getStu_author() {
+    return stu_author;
+  }
+
+  public void setStu_category(String stucategory) {
+    this.stu_category = stu_category;
+  }
+
+  public String getStu_category() {
+    return stu_category;
+  }
+
+}
---
+++ Library.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+public class Library {
+  Scanner sc = new Scanner(System.in);
+  Lib[] array = new Lib[100];
+  int num = 0;
+
+  // add operation
+  public void add() {
+    System.out.println("Enter title");
+    String title = sc.next();
+    System.out.println("Enter author");
+    String author = sc.next();
+    System.out.println("Enter category");
+    String category = sc.next();
+
+    Lib obj = new Lib();
+
+    obj.setStu_title(title);
+    obj.setStu_author(author);
+    obj.setStu_category(category);
+
+    array[num] = obj;
+    num++;
+
+    System.out.println(" Title: " + obj.getStu_title());
+    System.out.println("Author: " + obj.getStu_author());
+    System.out.println("Category: " + obj.getStu_category());
+
+    System.out.println("Number of Books" + num);
+  }
+
+  // Search operation
+  public void search() {
+    System.out.println("Enter books by title or author :");
+    String search = sc.next();
+    for (int i = 0; i < num; i++) {
+      if (search == array[i].getStu_title()) {
+        System.out.println("Author :" + array[i].getStu_author());
+        System.out.println("Category :" + array[i].getStu_category());
+      }
+    }
+  }
+
+  // Display books based on specific
+
+  public void display() {
+    System.out.println("Enter the category ");
+    String category = sc.next();
+    for (int i = 0; i < num; i++) {
+      if (category == array[i].getStu_category()) {
+        System.out.println("Title: " + array[i].getStu_title());
+        System.out.println("author :" + array[i].getStu_category());
+      }
+    }
+  }
+
+  // view operation
+  public void viewoptions() {
+    System.out.println("=============Library Application============== ");
+    int option = sc.nextInt();
+    do {
+      System.out.println("1.add book");
+      System.out.println("2.search for books by title or author");
+      System.out.println("3.Display books based on specific category");
+      System.out.println("4.Exit");
+      option = sc.nextInt();
+
+      if (option == 1) {
+        add();
+      }
+
+      else if (option == 2) {
+        search();
+      }
+
+      else if (option == 3) {
+        display();
+      }
+    } while (option != 4);
+
+  }
+
+  public static void main(String args[]) {
+    Library obj = new Library();
+    obj.viewoptions();
+
+  }
+}
